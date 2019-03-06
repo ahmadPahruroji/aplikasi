8b24c3f00cf0aeada429a62d30cdae602de8c9ad
netalinera <netainara@gmail.com>
2019-03-06 09:09:36 +0700

    baru

--- resources/views/layouts/coordinator.blade.php
+++ resources/views/layouts/coordinator.blade.php
@@ -126,18 +126,18 @@
                      </div>
                    </li>
 
-                   <li class="list-inline-item dropdown notif">
+                   <!-- <li class="list-inline-item dropdown notif">
                     <a class="nav-link dropdown-toggle arrow-none" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
                       <i class="fa fa-fw fa-bell-o"></i><span class="notif-bullet"></span>
                     </a>
                     <div class="dropdown-menu dropdown-menu-right dropdown-arrow dropdown-lg">
-                      <!-- item-->
-                      <div class="dropdown-item noti-title">
+                       <-- item-->
+                     <!--  <div class="dropdown-item noti-title">
                        {{--  <h5><small><span class="label label-danger pull-xs-right">5</span>Allerts</small></h5> --}}
-                     </div>
+                     </div> --> 
 
                      <!-- item-->
-                     <a href="#" class="dropdown-item notify-item">
+                     <!-- <a href="#" class="dropdown-item notify-item">
                       <div class="notify-icon bg-faded">
                        {{--  <img src="{{ asset('Admin/assets/images/avatars/avatar2.png') }}" alt="img" class="rounded-circle img-fluid"> --}}
                      </div>
@@ -146,10 +146,10 @@
                             <span>User registration</span>
                             <small class="text-muted">3 minutes ago</small>
                           </p> --}}
-                        </a>
+                        </a> -->
 
                         <!-- item-->
-                        <a href="#" class="dropdown-item notify-item">
+                        <!-- <a href="#" class="dropdown-item notify-item">
                           <div class="notify-icon bg-faded">
                            {{--  <img src="{{ asset('Admin/assets/images/avatars/avatar3.png') }}" alt="img" class="rounded-circle img-fluid"> --}}
                          </div>
@@ -158,10 +158,10 @@
                             <span>Task 2 completed</span>
                             <small class="text-muted">12 minutes ago</small>
                           </p> --}}
-                        </a>
+                        </a> -->
 
                         <!-- item-->
-                        <a href="#" class="dropdown-item notify-item">
+                       <!--  <a href="#" class="dropdown-item notify-item">
                           <div class="notify-icon bg-faded">
                            {{--  <img src="{{ asset('Admin/assets/images/avatars/avatar4.png') }}" alt="img" class="rounded-circle img-fluid"> --}}
                      {{--    </div>
@@ -170,15 +170,15 @@
                             <span>New job completed</span>
                             <small class="text-muted">35 minutes ago</small>
                           </p> --}}
-                        </a>
+                        </a> -->
 
                         <!-- All-->
-                        <a href="#" class="dropdown-item notify-item notify-all">
+                        <!-- <a href="#" class="dropdown-item notify-item notify-all">
                           {{-- View All Allerts --}}
                         </a>
 
                       </div>
-                    </li>
+                    </li> -->
 
                     <li class="list-inline-item dropdown notif">
                       <a class="nav-link dropdown-toggle nav-user" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
